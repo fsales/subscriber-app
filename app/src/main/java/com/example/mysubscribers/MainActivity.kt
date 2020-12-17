bf0e8900ca566b51e81e8a89d864b6d69472144c
Fábio Sales <fabio.oliveira.sales@gmail.com>
2020-12-16 21:49:16 -0300

    implementação da listagem.

--- app/src/main/java/com/example/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/example/mysubscribers/MainActivity.kt
@@ -13,14 +13,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setSupportActionBar(app_toolbar)
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
